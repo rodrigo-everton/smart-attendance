716e9a084e97bd89cab8922c854f65bde9a55c05
Rodrigo Everton <rodrigoeverton1500@gmail.com>
2025-11-08 12:59:27 -0300

    pagina Aluno

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,16 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Aluno from "./pages/Aluno";
-import FAQ from "./pages/FAQ";
-import Historico from "./pages/HistoricoChamada";
-import Presenca from "./pages/Presenca";
+import Home from "./pages/Home/Home";
+import Aluno from "./pages/Aluno/Aluno";
+import FAQ from "./pages/FAQ/FAQ";
+import Historico from "./pages/HistoricoChamada/HistoricoChamada";
+import Presenca from "./pages/Presenca/Presenca";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
-      <Header />
       <main className="min-h-screen p-4">
         <Routes>
           <Route path="/" element={<Home />} />
